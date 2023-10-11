c79374b9d0815c896137c78c79a1eb108cf861bc
ImVladislav <nafanya102@gmail.com>
2023-10-11 22:00:15 +0300

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
     .then(() => {
-        app.listen(3001, () => {
+        app.listen(3002, () => {
             console.log("Server running. Use our API on port: 3000")
 
         })
