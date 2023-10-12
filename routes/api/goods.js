c84ab321fd8715c3e1e0d68a3e2c495b17b8a6c1
ImVladislav <nafanya102@gmail.com>
2023-10-12 11:30:11 +0300

    noAuth

--- routes/api/goods.js
+++ routes/api/goods.js
@@ -2,25 +2,67 @@ const express = require("express");
 
 const ctrl = require("../../controllers/goods");
 
-const {validateBody, isValidId, authenticate} = require("../../middlewares");
+const {validateBody, isValidId, } = require("../../middlewares");
 
 const {schemas} = require("../../models/goods");
 
 const router = express.Router();
 
-router.get("/", authenticate, ctrl.getAll);
+router.get("/",  ctrl.getAll);
 
-router.get("/:id", authenticate, isValidId, ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
+router.post("/",  validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
-router.patch("/:id/checked", authenticate, isValidId, validateBody(schemas.updateChekedSchema), ctrl.updateCheked);
+router.patch("/:id/checked",  isValidId, validateBody(schemas.updateChekedSchema), ctrl.updateCheked);
 
-router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
+router.delete("/:id",  isValidId, ctrl.deleteById);
 
 module.exports = router;
+
+
+// const express = require("express");
+
+// const ctrl = require("../../controllers/goods");
+
+// const {validateBody, isValidId, authenticate} = require("../../middlewares");
+
+// const {schemas} = require("../../models/goods");
+
+// const router = express.Router();
+
+// router.get("/", authenticate, ctrl.getAll);
+
+// router.get("/:id", authenticate, isValidId, ctrl.getById);
+
+// router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.add);
+
+// router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+
+// router.patch("/:id/checked", authenticate, isValidId, validateBody(schemas.updateChekedSchema), ctrl.updateCheked);
+
+// router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
