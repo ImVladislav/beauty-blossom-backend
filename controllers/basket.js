5cb79a98e02d868e4b9a4ab571179d501f66fd74
Vladislav <nafanya102@gmail.com>
2024-06-14 13:44:08 +0300

    add basket reset

--- controllers/basket.js
+++ controllers/basket.js
@@ -1,100 +1,122 @@
 // const wood = require("../WoodStorage/wood")
 
-const { basket } = require('../models/basket')
+const { basket } = require("../models/basket");
+const { User } = require("../models/user");
 
+const cron = require("node-cron");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
-    const {_id: owner} = req.user; // щоб отримува тіоьки той хто створив
-    // const {page = 1, limit = 10} = req.query;
-    // req.query обєкт параметрів пошуку
-    // const skip = (page - 1) * limit;
+  const { _id: owner } = req.user; // щоб отримува тіоьки той хто створив
+  // const {page = 1, limit = 10} = req.query;
+  // req.query обєкт параметрів пошуку
+  // const skip = (page - 1) * limit;
 
-    // const result = await inProgressDesk.find();
-    // const result = await inProgressDesk.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
-       
-    const result = await basket.find({owner}, "-createdAt -updatedAt").populate("owner", "name email");
-    // -createdAt -updatedAt поля які не треба брати з бази
-    // populate бере айді знаходить овенра і вставляє обєкт з його данними
-    // 2 арг список полів які треба повернути
-    // skip скілеи пропустити обєктів в базі, limit скільки повернути
-    res.json(result);
-}
+  // const result = await inProgressDesk.find();
+  // const result = await inProgressDesk.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
+
+  const result = await basket
+    .find({ owner }, "-createdAt -updatedAt")
+    .populate("owner", "name email");
+  // -createdAt -updatedAt поля які не треба брати з бази
+  // populate бере айді знаходить овенра і вставляє обєкт з його данними
+  // 2 арг список полів які треба повернути
+  // skip скілеи пропустити обєктів в базі, limit скільки повернути
+  res.json(result);
+};
 
 const getById = async (req, res) => {
-    const { id } = req.params;
-    // const result = await Book.findOne({_id: id})
-    const result = await basket.findById(id);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
-}
+  const { id } = req.params;
+  // const result = await Book.findOne({_id: id})
+  const result = await basket.findById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
 const add = async (req, res) => {
-    const { _id: owner } = req.user;
-    const result = await basket.create({ ...req.body, owner });
-    //  const result = await Wood.create({...req.body});
+  const { _id: owner } = req.user;
+  const user = await User.findById(owner);
 
-    res.status(201).json(result);
+  if (!user.basketCreatedAt) {
+    user.basketCreatedAt = new Date();
+    await user.save();
+  }
 
-}
+  const result = await basket.create({ ...req.body, owner });
+  res.status(201).json(result);
+};
 
 const updateById = async (req, res) => {
   const { id } = req.params;
   const { quantity } = req.body;
 
-    const currentItem = await basket.findById(id);
+  const currentItem = await basket.findById(id);
 
-    if (!currentItem) {
-      throw HttpError(404, "Not found");
-    }
-    currentItem.quantity = parseInt(quantity);
-    await currentItem.save();
+  if (!currentItem) {
+    throw HttpError(404, "Not found");
+  }
+  currentItem.quantity = parseInt(quantity);
+  await currentItem.save();
 
-    res.json(currentItem);
-
+  res.json(currentItem);
 };
 
 const updateCheked = async (req, res) => {
-    const { id } = req.params;
-    const result = await basket.findByIdAndUpdate(id, req.body, {new: true});
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
-}
+  const { id } = req.params;
+  const result = await basket.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
 const deleteById = async (req, res) => {
-    const { id } = req.params;
-    const result = await basket.findByIdAndRemove(id);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json({
-        message: "Delete success"
-    })
-}
+  const { id } = req.params;
+  const result = await basket.findByIdAndRemove(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
 
 const deleteAllByOwner = async (req, res) => {
-    const { _id: owner } = req.user;
+  const { _id: owner } = req.user;
 
-    const result = await basket.deleteMany({ owner });
+  const result = await basket.deleteMany({ owner });
 
-    res.json({
-        message: "All items deleted successfully",
-        deletedCount: result.deletedCount,
-    });
-}
+  res.json({
+    message: "All items deleted successfully",
+    deletedCount: result.deletedCount,
+  });
+};
 
+cron.schedule("0 0 * * *", async () => {
+  // 00:00:00 every day
+  // cron.schedule("*/10 * * * * *", async () => {
+  // every 10 seconds
+  try {
+    const now = new Date();
+    // const cutoffDate = new Date(now.getTime() - 30 * 1000); // 30 секунд тому
+    const cutoffDate = new Date(now.getTime() - 5 * 24 * 60 * 60 * 1000);
+    // Видалити всі записи, створені більше ніж 30 секунд тому
+    const result = await basket.deleteMany({ createdAt: { $lt: cutoffDate } });
+    console.log(`Deleted ${result.deletedCount} items from the basket.`);
+  } catch (error) {
+    console.error("Error deleting expired basket items:", error);
+  }
+});
 
 module.exports = {
-    deleteAllByOwner: ctrlWrapper(deleteAllByOwner),
-    getAll: ctrlWrapper(getAll),
-    getById: ctrlWrapper(getById),
-    add: ctrlWrapper(add),
-    updateById: ctrlWrapper(updateById),
-    updateCheked: ctrlWrapper(updateCheked),
-    deleteById: ctrlWrapper(deleteById),
-}
+  deleteAllByOwner: ctrlWrapper(deleteAllByOwner),
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  updateCheked: ctrlWrapper(updateCheked),
+  deleteById: ctrlWrapper(deleteById),
+};
--- models/basket.js
+++ models/basket.js
@@ -1,13 +1,14 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 const Joi = require("joi");
-const { handleMongooseError } = require('../helpers');
+const { handleMongooseError } = require("../helpers");
 
 // const statusList = ['на складі', 'в роботі'];
-const basketShema = new Schema({
-        owner: {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: false,
+const basketShema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: false,
     },
     name: String,
     article: String,
@@ -21,13 +22,24 @@ const basketShema = new Schema({
     brand: String,
     images: String,
     new: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     sale: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+        },
+    
+    basketCreatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+
     category: String,
     subCategory: String,
     subSubCategory: String,
@@ -37,43 +49,46 @@ const basketShema = new Schema({
     //     enum: statusList,
     //     required: true,
     // }
-}, { versionKey: false, timestamps: true });
+  },
+  { versionKey: false, timestamps: true }
+);
 
-basketShema.post('save', handleMongooseError);
+basketShema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
-    name: Joi.string().required(),
-    article: Joi.string().required(),
-    productId: Joi.number().required(),
-    code: Joi.number().required(),
-    amount: Joi.number(),
-    description: Joi.string().required(),
-    priceOPT: Joi.number().required(),
-    quantity: Joi.number().required(),
-    price: Joi.number().required(),
-    brand: Joi.string().required(),
-    images: Joi.string().required(),
-    new: Joi.boolean().default(false),
-    sale: Joi.boolean().required(),
-    // status: Joi.string().valid(...statusList).required(),
-    category: Joi.string(),
-    subCategory: Joi.string(),
-    subSubCategory: Joi.string(),
+  name: Joi.string().required(),
+  article: Joi.string().required(),
+  productId: Joi.number().required(),
+  code: Joi.number().required(),
+  amount: Joi.number(),
+  description: Joi.string().required(),
+  priceOPT: Joi.number().required(),
+  quantity: Joi.number().required(),
+  price: Joi.number().required(),
+  brand: Joi.string().required(),
+  images: Joi.string().required(),
+  new: Joi.boolean().default(false),
+  sale: Joi.boolean().required(),
+  // status: Joi.string().valid(...statusList).required(),
+  category: Joi.string(),
+  subCategory: Joi.string(),
+  subSubCategory: Joi.string(),
+  createdAt: Joi.date(),
+  basketCreatedAt: Joi.date(),
 });
 
-
 const updateChekedSchema = Joi.object({
-    checked: Joi.boolean().required(),
+  checked: Joi.boolean().required(),
 });
 
 const schemas = {
-    addSchema,
-    updateChekedSchema,
+  addSchema,
+  updateChekedSchema,
 };
 
 const basket = model("basket", basketShema);
 
 module.exports = {
-    basket,
-    schemas,
+  basket,
+  schemas,
 };
