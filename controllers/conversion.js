f88ddaf2d3c61819318c5a6ee73746a2b8a30b5e
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-08-01 18:17:29 +0300

    Fixed conversion params case

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 
 const send = async (req, res) => {
-	const {eventName, eventTime, eventId, eventSourceUrl, userData = {}} = req.body;
+	// eslint-disable-next-line camelcase
+	const {event_name, event_time, event_id, event_source_url, user_data = {}} = req.body;
 
 	const pixelId = '789745059892711';
 	const accessToken = 'EAA04OeuiMK8BO9ibuOGZCCgfOhpEMUhdsoiKK1VlNgWVUA7LB43A9bwpJqdAQyRzYuJxpAx7Ad63pDU2ZClOmerZAXTZBAQB6S2oyMgD08vRCJrsZAIa3cQapueQGRZBSGUUwKMuKX6xW9AvJq38ERWpyRnZAnpv67HFaJY3qHzLxr62ZCJ7CZBjjfsKX0WCPvlBEaQZDZD';
@@ -9,13 +10,14 @@ const send = async (req, res) => {
 	const payload = {
 		data: [
 			{
-				event_name:       eventName,
-				event_time:       eventTime,
-				event_id:         eventId,
-				event_source_url: eventSourceUrl,
+				event_name:       event_name,
+				event_time:       event_time,
+				event_id:         event_id,
+				event_source_url: event_source_url,
 				action_source:    "website",
 				user_data:        {
-					...userData,
+					// eslint-disable-next-line camelcase
+					...user_data,
 					client_user_agent: req.headers["user-agent"],
 					client_ip_address: req.ip,
 				},
