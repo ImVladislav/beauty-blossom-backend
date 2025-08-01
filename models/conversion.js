54eaadadf9d5755c4fe2c1941914453f3eaf03c4
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-08-01 19:23:31 +0300

    Fixed conversion custom data schema

--- models/conversion.js
+++ models/conversion.js
@@ -6,6 +6,16 @@ const sendSchema = Joi.object({
 	event_id:         Joi.string().required(),
 	event_source_url: Joi.string().uri().required(),
 	user_data:        Joi.object().optional(),
+	// eslint-disable-next-line no-use-before-define
+	custom_data:      customDataSchema.optional(),
+});
+
+const customDataSchema = Joi.object({
+	currency:     Joi.string().length(3).uppercase().optional(),
+	value:        Joi.number().optional(),
+	content_ids:  Joi.array().items(Joi.string()).optional(),
+	content_name: Joi.string().optional(),
+	content_type: Joi.string().optional(),
 });
 
 const schemas = {
