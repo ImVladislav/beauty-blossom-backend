dc2580ae2188d885afed93c346b81b78e1bbb62f
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-08-01 18:39:55 +0300

    Added custom fields to conversation controller

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const send = async (req, res) => {
 	// eslint-disable-next-line camelcase
-	const {event_name, event_time, event_id, event_source_url, user_data = {}} = req.body;
+	const {event_name, event_time, event_id, event_source_url, user_data = {}, custom_data = {}} = req.body;
 
 	const pixelId = '789745059892711';
 	const accessToken = 'EAA04OeuiMK8BO9ibuOGZCCgfOhpEMUhdsoiKK1VlNgWVUA7LB43A9bwpJqdAQyRzYuJxpAx7Ad63pDU2ZClOmerZAXTZBAQB6S2oyMgD08vRCJrsZAIa3cQapueQGRZBSGUUwKMuKX6xW9AvJq38ERWpyRnZAnpv67HFaJY3qHzLxr62ZCJ7CZBjjfsKX0WCPvlBEaQZDZD';
@@ -21,6 +21,7 @@ const send = async (req, res) => {
 					client_user_agent: req.headers["user-agent"],
 					client_ip_address: req.ip,
 				},
+				...(Object.keys(custom_data).length > 0 && {custom_data}),
 			},
 		],
 	};
@@ -31,7 +32,7 @@ const send = async (req, res) => {
 		res.status(200).json({success: true, fb_response: response.data});
 	} catch (err) {
 		console.error("Facebook Conversions API error:", err.response?.data || err.message);
-		res.status(500).json({error: "Failed to send conversion event"});
+		res.status(500).json({error: "Failed to send conversion event", details: err.response?.data || err.message});
 	}
 };
 
