dac1897d29a024823c94f961380183de9500d312
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-08-18 20:02:35 +0300

    Updated mailer params

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,37 +1,32 @@
 const nodemailer = require("nodemailer");
 const sendTelegramMessage = require("../helpers/telegram");
-const {EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS} = process.env;
+const {EMAIL_PORT, EMAIL_USER} = process.env;
 
 const transporter = nodemailer.createTransport({
-  host: "mail.adm.tools",
-  port: EMAIL_PORT,
-  secure: true,
-  auth: {
-    user: EMAIL_USER,
-    pass: EMAIL_PASS,
-  },
+	host:   "mail.adm.tools",
+	port:   EMAIL_PORT,
+	secure: true,
+	auth:   {
+		user: EMAIL_USER,
+		pass: 'Adminnimda',
+	},
 });
 
 const mailer = async (message) => {
-  try {
-    await transporter.sendMail({
-      ...message,
-      from: EMAIL_USER,
-    });
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-	await sendTelegramMessage(
-		`❌ Помилка (mailer): ${error.message}\n\n` +
-		`Stack:\n${error.stack}\n\n` +
-		`Env:\n` +
-		`HOST: ${EMAIL_HOST}\n` +
-		`PORT: ${EMAIL_PORT}\n` +
-		`USER: ${EMAIL_USER}\n` +
-		`PASS: ${EMAIL_PASS}`
-	);
-    throw new Error("Error sending email");
-  }
+	try {
+		await transporter.sendMail({
+			...message,
+			from: EMAIL_USER,
+		});
+		console.log("Email sent successfully");
+	} catch (error) {
+		console.error("Error sending email:", error);
+		await sendTelegramMessage(
+			`❌ Помилка (mailer): ${error.message}\n\n` +
+			`Stack:\n${error.stack}\n\n`
+		);
+		throw new Error("Error sending email");
+	}
 };
 
 module.exports = mailer;
