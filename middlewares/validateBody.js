b2cd71f7ee10faf64f2b92f32765b856b4cac9e2
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-10-30 23:09:50 +0200

    [FIX] Allowed empty strings for body

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,12 @@
 const {HttpError} = require("../helpers");
 
+const joiOptions = {
+	allowUnknown: true,
+};
+
 const validateBody = schema => {
 	const func = (req, res, next) => {
-		const {error} = schema.validate(req.body);
+		const {error} = schema.validate(req.body, joiOptions);
 		if (error) {
 			return next(HttpError(400, error.message));
 		}
